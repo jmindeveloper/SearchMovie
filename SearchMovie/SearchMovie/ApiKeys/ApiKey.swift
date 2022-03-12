bc2eab6d2996aa271d93a653f6f818b19c615a47
jmindevelper <jmin.developer@gmail.com>
2022-03-12 14:11:50 +0900

    프로젝트 생성

---
+++ SearchMovie/SearchMovie/ApiKeys/ApiKey.swift
@@ -0,0 +1,14 @@
+//
+//  ApiKey.swift
+//  SearchMovie
+//
+//  Created by J_Min on 2022/03/12.
+//
+
+import Foundation
+
+struct ApiKey {
+    let naverClientID = "SPb5FVaFuJIniLi1owNo"
+    let naverClientSecret = "rBPja6I3ot"
+    let koficApiKey = "6e1adfd9fd6b527fa5a662c5fba30a48"
+}
---
+++ SearchMovie/SearchMovie/SearchMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchMovieApp.swift
+//  SearchMovie
+//
+//  Created by J_Min on 2022/03/12.
+//
+
+import SwiftUI
+
+@main
+struct SearchMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
